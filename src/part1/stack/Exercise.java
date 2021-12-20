5cd2dd1aeb96d62c4801dbb42b2d701f1a74d1eb
Mukhammadyaqub <muhammadyoqub@gmail.com>
2021-12-21 00:05:19 +0500

    Implementations standard methods of Stack

--- src/part1/stack/Exercise.java
+++ src/part1/stack/Exercise.java
@@ -1,5 +1,7 @@
 package part1.stack;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -9,7 +11,18 @@ import java.util.Stack;
  * Software team
  */
 public class Exercise {
+
+	private final static List<Character> leftBrackets = Arrays.asList('(', '{', '[', '<');
+	private final static List<Character> rightBrackets = Arrays.asList(')', '}', ']', '>');
+
 	public static void main(String[] args) {
+		part1.stack.Stack stack = new part1.stack.Stack();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		System.out.println(stack.pop());
+		System.out.println(stack.peek());
+		System.out.println(stack);
 		System.out.println(reverse("!tcerroC"));
 		System.out.println("isValidBrackets : " + isValidBrackets("{12<>}(22[(())])[]"));
 	}
@@ -35,9 +48,9 @@ public class Exercise {
 
 		Stack<Character> stack = new Stack<>();
 		for (char ch : input.toCharArray()) {
-			if (isOpening(ch))
+			if (isLeftBracket(ch))
 				stack.push(getPartner(ch));
-			else if (isClosing(ch)) {
+			else if (isRightBracket(ch)) {
 				if (stack.isEmpty() || stack.pop() != ch) {
 					return false;
 				}
@@ -47,15 +60,15 @@ public class Exercise {
 		return stack.isEmpty();
 	}
 
-	private static boolean isOpening(char ch) {
-		return ch == '(' || ch == '{' || ch == '[' || ch == '<';
+	private static boolean isLeftBracket(char ch) {
+		return leftBrackets.contains(ch);
 	}
 
-	private static boolean isClosing(char ch) {
-		return ch == ')' || ch == '}' || ch == ']' || ch == '>';
+	private static boolean isRightBracket(char ch) {
+		return rightBrackets.contains(ch);
 	}
 
 	private static char getPartner(char ch) {
-		return ch == '(' ? ')' : ch == '{' ? '}' : ch == '[' ? ']' : '>';
+		return rightBrackets.get(leftBrackets.indexOf(ch));
 	}
 }
---
+++ src/part1/stack/Stack.java
@@ -0,0 +1,39 @@
+package part1.stack;
+
+import java.util.EmptyStackException;
+
+public class Stack {
+	private int[] items = new int[10];
+	private int size;
+
+	public void push(int item) {
+		if (size == items.length)
+			throw new StackOverflowError();
+
+		items[size++] = item;
+	}
+
+	public int pop() {
+		if (isEmpty()) throw new EmptyStackException();
+		return items[--size];
+	}
+
+	public int peek() {
+		if (isEmpty()) throw new EmptyStackException();
+		return items[size - 1];
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuffer = new StringBuilder();
+		stringBuffer.append("[");
+		for (int i = 0; i < size; i++)
+			stringBuffer.append(items[i]).append(i == size - 1 ? "" : ",");
+		stringBuffer.append("]");
+		return stringBuffer.toString();
+	}
+}
